c4a563d54a57f89ee0dbe4601819765fd7926fb0
hermes829 <hermes829@gmail.com>
2014-12-26 22:11:09 -0500

    perf stats

--- Analysis/Supervised/bestModel.R
+++ Analysis/Supervised/bestModel.R
@@ -42,6 +42,7 @@ for(gram in grams){
 		topLevel=char(txt[8:11,])
 		stats=unlist(lapply(strsplit(topLevel, ': '), function(x) num(x[2])))
 		
+		if(cat==''){cat=4}
 		data=cbind(gram, cat, rbind(stats))
 		perfData=rbind(perfData, data)
 		}
@@ -57,7 +58,7 @@ perfData=convNumDcol(perfData, 3:6)
 
 # Order position of gram cats
 perfData$order=apply(perfData, 1, function(x) mean(num(x[3:6])))
-perfData$gramCat=paste(perfData$gram, perfData$cat, sep='__')
+perfData$gramCat=paste(perfData$gram, perfData$cat, sep='_&_')
 gramCatOrder=perfData$gramCat[order(perfData$order, decreasing=TRUE)]
 perfData$gramCat=factor(perfData$gramCat, levels=gramCatOrder)
 
@@ -74,3 +75,36 @@ tmp
 
 ## Class level performance
 ### Compare polcat3, polCat, polCat7 within grams
+getClassData=function(gram, cat){
+	texName=paste0('polCat', cat, '_train99-08_test09-13.txt')
+	txt=read.table(texName, sep='\n', strip.white=TRUE)
+
+
+	classLevel=lapply(14:(nrow(txt)-1), function(x){
+		splits=strsplit(char(txt[x,]), ' ')[[1]]
+		num(splits[splits!=''])
+		})
+	classLevel=do.call('rbind', classLevel)
+	colnames(classLevel)=c('class', 'precision', 'recall', 'f1', 'support')
+	classLevel=data.frame(classLevel)
+	classLevel$gramCat=paste0('gram: ', gram, ' cat: ', cat)
+	return(classLevel)
+}
+
+# Reshape fo rplotting
+classData=rbind( 
+	getClassData('1_3', 3)[,c(1:4, 6)], getClassData('1_3', '')[,c(1:4, 6)], getClassData('1_3', 7)[,c(1:4, 6)],
+	getClassData('1_4', 3)[,c(1:4, 6)], getClassData('1_4', '')[,c(1:4, 6)], getClassData('1_4', 7)[,c(1:4, 6)],
+	getClassData('1_5', 3)[,c(1:4, 6)], getClassData('1_5', '')[,c(1:4, 6)], getClassData('1_5', 7)[,c(1:4, 6)]
+	)
+ggData=melt(classData, id=c('class', 'gramCat'))
+
+tmp=ggplot(ggData, aes(y=class, x=variable)) 
+tmp=tmp + ylab('Class') + xlab('Statistic')
+tmp=tmp + scale_y_continuous(breaks=1:max(ggData$class), labels=1:max(ggData$class))
+tmp=tmp + geom_tile(aes(fill=value), colour='white')
+tmp=tmp + facet_wrap(~ gramCat, scales='free', ncol=3)
+tmp=tmp + theme(axis.ticks=element_blank(), 
+  legend.position='top', legend.key.width=unit(2,'cm'),
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank() )
+tmp

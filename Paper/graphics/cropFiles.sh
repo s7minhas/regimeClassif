5c5caafdbfd85a2be4a9102bcc6ccfeb26a7bad3
hermes829 <hermes829@gmail.com>
2015-01-30 12:28:05 -0500

    cropping

--- Paper/graphics/cropFiles.sh
+++ Paper/graphics/cropFiles.sh
@@ -2,7 +2,7 @@ pdfcrop svmIntro.pdf svmIntro.pdf
 
 pdfcrop allAggPerf.pdf allAggPerf.pdf
 
-pdfcrop polCat_cat4_All_sep.pdf polCat_cat4_All_sep.pdf 
+pdfcrop polGe7_All_sep.pdf polGe7_All_sep.pdf 
 pdfcrop military_All_sep.pdf military_All_sep.pdf
 pdfcrop monarchy_All_sep.pdf monarchy_All_sep.pdf
 pdfcrop party_All_sep.pdf party_All_sep.pdf
@@ -13,7 +13,9 @@ pdfcrop military_perfChange.pdf military_perfChange.pdf
 
 pdfcrop pol_bin_wrdCloud.pdf pol_bin_wrdCloud.pdf
 
-pdfcrop polCat_cat4_2010_map.pdf polCat_cat4_2010_map.pdf
-pdfcrop military_2010_map.pdf military_2010_map.pdf
-pdfcrop monarchy_2010_map.pdf monarchy_2010_map.pdf
-pdfcrop party_2010_map.pdf party_2010_map.pdf
+pdfcrop pol_bin_probDist.pdf pol_bin_probDist.pdf
+
+pdfcrop polGe7_2009_map.pdf polGe7_2009_map.pdf
+pdfcrop military_2009_map.pdf military_2009_map.pdf
+pdfcrop monarchy_2009_map.pdf monarchy_2009_map.pdf
+pdfcrop party_2009_map.pdf party_2009_map.pdf
